f24094e0b08d79c585dd47921c9d372a9444a339
Mariyamsalman <salmanmariyam47@gmail.com>
2025-07-31 13:13:01 +0500

    javascript lec 3 (variables)

--- javascript.js
+++ javascript.js
@@ -1,3 +1,63 @@
+/*
+---function for external js----
 function info(){
  alert('this button is clicked')   
 }
+ ***************************************************************
+ ---------variables:var--------
+var variable1=10;
+console.log(variable1);
+*redeclaration of variable of var
+var variable1=30;
+console.log(variable1);
+*reinitiallization of var variable
+variable1=20;
+console.log(variable1);
+**************************************************************
+
+-----------let variable----------
+{
+    let variable2=10;
+   //
+ console.log(variable2);
+    variable2=20;     =>update of let variable
+   // console.log(variable2);
+}
+console.log(variable2);  =>not accesible beacuse let variable is block scoped.
+
+*use of same let variables in different scopes
+{
+    let variable1=10;
+   
+ console.log(variable1);
+}
+{
+    let variable1=20   
+     console.log(variable1);
+} 
+********************************************
+
+----------------const variable----------------------
+const country="pakistan";
+console.log(country);
+country="america"; => it will give error beacause const variable cannot be update
+*********************************************************************
+quiz 2
+-------------------Q1----------------
+ const age=18;
+ *********************
+ -----------------Q2-------------------
+ let name="mariyam";
+ var city="karachi";
+ console.log(age)
+ console.log(name)
+  console.log(city)
+  ****************************
+
+---------------------Q3--------------------
+  function info(){
+    console.log("mariyam")
+    alert("external js is working")
+  } 
+    *******************************
+     */
